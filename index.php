09e6abbe4b49954f6a07aaafca7b93cb9d9fa017
Thusharsp <Thusharsp@gmail.com>
2022-07-11 10:29:23 +0530

    forst-commit

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COFFEE STORE</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="style2.css">
+
+</head>
+<body>
+<header class="header">
+
+    <a href="#" class="logo">
+        <img src="images/logo.png" alt="">
+    </a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="product_index.php">Products</a>
+    </nav>
+
+    <div class="icons">
+        <a href='cart.php'><div class="fas fa-shopping-cart" id="cart-btn"></div></a>
+        <div class="fas fa-bars" id="menu-btn"></div>
+    </div>
+
+</header>
+<section class="home" id="home">
+
+    <div class="content">
+        <h3>FRESH COFFEE ON YOUR DOOR STEPS</h3>
+        <p></p>
+        <a href="product_index.php" class="btn">get yours now</a>
+    </div>
+
+</section>
+<section class="about" id="about">
+
+    <h1 class="heading"> <span>about</span> us </h1>
+
+    <div class="row">
+
+        <div class="image">
+            <img src="images/about-img.jpeg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>What we do?</h3>
+            <p>We help you connect direclty to coffee sellers! So that you can experience the taste of real filter coffee!</p>
+            <p>Get your fresh non chemicaled coffee powder on your door steps now!!!!!</p>
+            <a href="product_index.php" class="btn">Check our products</a>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="footer">
+
+    <div class="share">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        <a href="#" class="fab fa-pinterest"></a>
+    </div>
+    <div class="credit">Deveoped by <span>Thushar and Prathika</span></div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
